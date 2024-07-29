538f2998288d13ba525f4cb7f9b2d7d73b6c7862
Ariel Videla <ariel_videla@hotmail.com>
2024-07-29 10:52:46 -0300

    about4

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -159,7 +159,7 @@ const About = () => {
       </motion.div>
       <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
         {/* text */}
-        <div className="flex-1 flex flex-col justify-center xl:mt-0 sm:mt-[-60px]">
+        <div className="flex-1 flex flex-col justify-center xl:mt-0 sm:mt-[-75px]">
           <motion.h2
             variants={fadeIn("right", 0.2)}
             initial="hidden"
